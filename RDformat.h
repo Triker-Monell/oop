7003be80654f1e1e96fad553cee35068492dc003
monell <monell.zhong@gmail.com>
2018-05-19 15:11:05 +0800

    debug

--- RDformat.h
+++ RDformat.h
@@ -22,7 +22,7 @@ class RottenTomatoesRDformat:public RDformat{
     const static std::string MONTH[]={
             "","Jan","Feb","Mar","Apr","May","June",
             "July","Aug","Sept","Oct","Nov","Dec"
-    }
+    };
 public:
     std::tuple<int,int,int,std::string> setRD(std::string newData);
     std::string getRD(int year,int month,int day,std::string CountryorRegion);
