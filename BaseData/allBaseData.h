0d4fc66f07e6b47264059d550e7e6473285cf4cc
monell <monell.zhong@gmail.com>
2018-06-29 13:44:40 +0800

    all headers

---
+++ BaseData/allBaseData.h
@@ -0,0 +1,39 @@
+#ifndef ALLBASEDATA_H
+#define ALLBASEDATA_H
+
+
+#include "BaseData.h"
+
+#include "Format/DateFormat.h"
+#include "Format/DBSio.h"
+#include "Format/IMDBSio.h"
+#include "Format/RTSio.h"
+#include "Format/IOFormat.h"
+
+
+#include "Boxing.h"
+#include "Date.h"
+#include "Director.h"
+#include "DouBanScore.h"
+#include "Episodes.h"
+//#include "Image.h"
+#include "IMDBScore.h"
+#include "Intro.h"
+#include "Language.h"
+#include "LeadingActor.h"
+#include "Name.h"
+#include "OfficialSites.h"
+#include "People.h"
+#include "Place.h"
+#include "Rated.h"
+#include "RottenTomatoesScore.h"
+#include "Runtime.h"
+#include "Score.h"
+#include "Similar.h"
+#include "Type.h"
+#include "Writer.h"
+
+
+
+
+#endif
---
+++ Catcher/allCatcher.h
@@ -0,0 +1,29 @@
+#ifndef ALLCATCHER_H
+#define ALLCATCHER_H
+
+#include "BaseCatcher.h"
+
+#include "Douban_by_movies.h"
+#include "Douban_by_people.h"
+#include "Douban_by_toplist.h"
+#include "Douban_by_TV.h"
+
+
+
+#include "IMDB_by_movies.h"
+#include "IMDB_by_people.h"
+#include "IMDB_by_toplist.h"
+#include "IMDB_by_TV.h"
+
+
+
+#include "RottenTomatoes_by_movies.h"
+#include "RottenTomatoes_by_people.h"
+#include "RottenTomatoes_by_toplist.h"
+#include "RottenTomatoes_by_TV.h"
+
+
+
+
+
+#endif
