bc3401d9e7863ec75a83598203c951fbbcf3898e
monell <monell.zhong@gmail.com>
2018-07-01 02:35:07 +0800

    using

--- BaseData/Format/IOFormat.cpp
+++ BaseData/Format/IOFormat.cpp
@@ -5,6 +5,18 @@ void stdInput::input(std::vector<std::string> &vec, std::string data) {
         std::string tmp;
         while(i<data.size()&&data[i]!='/'&&data[i]!='|') tmp+=data[i++];
         //std::cout << tmp << std::endl;
+
+        //filter
+        int head=0,tail=tmp.size()-1;
+        while(head<tmp.size()&&(tmp[head]==' '||tmp[head]=='/'||tmp[head]=='|')){
+            head++;
+        }
+        while(tail>=0&&(tmp[head]==' '||tmp[head]=='/'||tmp[head]=='|')){
+            tail--;
+        }
+        if(head>tail) continue; //如果error
+
+        tmp=tmp.substr(head,tail-head+1);
         vec.push_back(tmp);
     }
 }
