bb2c8fbfa05e23d79668a1e764807b2b60907d79
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 22:32:23 +0800

    Update RottenTomatoes_by_movies.h

--- Catcher/RottenTomatoes_by_movies.h
+++ Catcher/RottenTomatoes_by_movies.h
@@ -2,6 +2,9 @@
 #define MICP_LANFANQIECATCHER_H
 
 #include "BaseCatcher.h"
+
+//类的具体含义详见Catcher说明文档
+//函数的功能如BaseCatcher中的说明所述
 class RottenTomatoes_by_movies:public BaseCatcher{
 public:
     RottenTomatoes_by_movies(){}
