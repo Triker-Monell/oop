45311f1c77ef6f3b31615001ba2d284192f172c4
monell <monell.zhong@gmail.com>
2018-07-01 16:37:04 +0800

    using

--- BaseData/Format/RTSio.cpp
+++ BaseData/Format/RTSio.cpp
@@ -1,11 +1,16 @@
 #include "RTSio.h"
 #include <sstream>
-
+#include <cctype>
 void RTSinput::input(std::vector<std::string> &vec, std::string data) {
     std::stringstream ss;
     ss << data;
     std::string fresh,ar;
     ss >> fresh >> ar;
+
+    int head=0,tail=ar.size()-1;
+    while(head<ar.size()&&!isdigit(ar[head])) head++;
+    while(tail>=0&&!isdigit(ar[tail])) tail--;
+    ar=ar.substr(head,tail-head+1);
     vec.push_back(fresh);
     vec.push_back(ar);
 }
