4de45e2a5aeeb128e79cc331365abd3869ad1931
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 22:12:01 +0800

    Update IMDB_by_people.cpp

--- Catcher/IMDB_by_people.cpp
+++ Catcher/IMDB_by_people.cpp
@@ -8,6 +8,8 @@ void IMDB_by_people::ExporttoDatabase() {
 
 }
 void IMDB_by_people::MakeCatcher() {
+    //该部分为使用C++中的python指针调用py工作
+    //每个函数只有在PyImport_ImportModule时不同，每个去调用名称对应的xxx.py
     PyObject * pModule = nullptr,*pFunc = nullptr;
     PyObject * pArgs = nullptr,*pValue = nullptr;
     Py_Initialize();
@@ -18,7 +20,7 @@ void IMDB_by_people::MakeCatcher() {
     qDebug() << QString::fromStdString(cmd);
 
     PyRun_SimpleString(cmd.c_str());
-    //????????
+ 
 
     pModule = PyImport_ImportModule("IMDB_by_people");
     pFunc = PyObject_GetAttrString(pModule, "work");
