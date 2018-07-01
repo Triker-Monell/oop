f0411d57757cb94550d895b91948c6403727814f
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 22:31:59 +0800

    Update RottenTomatoes_by_TV.h

--- Catcher/RottenTomatoes_by_TV.h
+++ Catcher/RottenTomatoes_by_TV.h
@@ -5,6 +5,8 @@
 #ifndef MICP_ROTTENTOMATOES_BY_TV_H
 #define MICP_ROTTENTOMATOES_BY_TV_H
 #include "BaseCatcher.h"
+//类的具体含义详见Catcher说明文档
+//函数的功能如BaseCatcher中的说明所述
 class RottenTomatoes_by_TV:public BaseCatcher{
 public:
     RottenTomatoes_by_TV(){}
