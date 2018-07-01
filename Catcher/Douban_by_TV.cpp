a8443a2ccd3179d106bb6946813050853438582e
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 19:38:49 +0800

    注释

--- Catcher/Douban_by_TV.cpp
+++ Catcher/Douban_by_TV.cpp
@@ -8,20 +8,24 @@ void Douban_by_TV::ExporttoDatabase() {
 
 }
 void Douban_by_TV::MakeCatcher() {
+    //该部分为使用C++中的python指针调用py工作
+    //每个函数只有在PyImport_ImportModule时不同，每个去调用名称对应的xxx.py
     PyObject * pModule = nullptr,*pFunc = nullptr;
     PyObject * pArgs = nullptr,*pValue = nullptr;
     Py_Initialize();
 
     PyRun_SimpleString("import sys");
+    
     PyRun_SimpleString("sys.path.append('/home/monell/qtcode/InfoCS/Catcher/')");
-    //????????
-
+   
+    
     pModule = PyImport_ImportModule("Douban_by_TV");
     pFunc = PyObject_GetAttrString(pModule, "work");
     pValue = PyObject_CallObject(pFunc,pArgs);
     Py_Finalize();
 }
 std::ifstream Douban_by_TV::SaveinBaseObject() {
+    
     std::ifstream readfile;
     readfile.open("/home/monell/qtcode/build-InfoCS-Desktop_Qt_5_10_1_GCC_64bit-Debug/Douban_by_TV.txt",std::ios::in);
     return readfile;
