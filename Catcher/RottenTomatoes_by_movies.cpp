686683a3959f96ce3d73aa0b42b4369343a668e1
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 19:45:48 +0800

    注释

--- Catcher/RottenTomatoes_by_movies.cpp
+++ Catcher/RottenTomatoes_by_movies.cpp
@@ -3,13 +3,15 @@ void RottenTomatoes_by_movies::ExporttoDatabase() {
 
 }
 void RottenTomatoes_by_movies::MakeCatcher() {
+    //该部分为使用C++中的python指针调用py工作
+    //每个函数只有在PyImport_ImportModule时不同，每个去调用名称对应的xxx.py
     PyObject * pModule = nullptr,*pFunc = nullptr;
     PyObject * pArgs = nullptr,*pValue = nullptr;
     Py_Initialize();
 
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append('/home/monell/qtcode/InfoCS/Catcher')");
-    //????????
+ 
 
     pModule = PyImport_ImportModule("RottenTomatoes_by_movies");
     pFunc = PyObject_GetAttrString(pModule, "work");
