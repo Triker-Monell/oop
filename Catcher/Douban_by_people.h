a2b87b0d5ea93717ebc02e0a24fa935898eb3d1f
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 22:29:59 +0800

    Update Douban_by_people.h

--- Catcher/Douban_by_people.h
+++ Catcher/Douban_by_people.h
@@ -4,6 +4,8 @@
 
 #ifndef MICP_DOUBAN_BY_PEOPLE_H
 #define MICP_DOUBAN_BY_PEOPLE_H
+//类的具体含义详见Catcher说明文档
+//函数的功能如BaseCatcher中的说明所述
 #include "BaseCatcher.h"
 class Douban_by_people:public BaseCatcher{
 public:
