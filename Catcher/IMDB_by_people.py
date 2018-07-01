d25a33c891de64c135a2ace09116e10848655d3c
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 22:40:23 +0800

    Update IMDB_by_people.py

--- Catcher/IMDB_by_people.py
+++ Catcher/IMDB_by_people.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf-8')
 def get_html(url):  #通过url获取网页内容
     result = urllib.urlopen(url)
     return result.read()
-    # save_file(result.read(), 'thefile.txt')
+    
 
 def get_movie_url(url):
     html=get_html(url)
@@ -61,20 +61,20 @@ def get_movie_one(movie,name):
     soup_all = BeautifulSoup(str(movie),"html.parser")
     title = soup_all.find_all('h1' ,class_="header")
     soup_title = BeautifulSoup(str(title[0]),"html.parser")
-    for line in soup_title.stripped_strings:  # 对获取到的<a>里的内容进行提取
+    for line in soup_title.stripped_strings:  
         result.append(line)
 
     result_str=""
 
     info = soup_all.find_all('div', id="name-born-info" ,class_="txt-block")
     soup_info = BeautifulSoup(str(info[0]),"html.parser")
-    for line in soup_info.stripped_strings:  # 对获取到的<a>里的内容进行提取
+    for line in soup_info.stripped_strings:   
         result_str=result_str+" "+line
 
     result_str=result_str+" job: "
     job= soup_all.find_all('div' ,class_="infobar", id="name-job-categories")
     soup_job = BeautifulSoup(str(job[0]),"html.parser")
-    for line in soup_job.stripped_strings:  # 对获取到的<span>里的内容进行提取
+    for line in soup_job.stripped_strings:  
         result_str = result_str + line
 
     result_str = result_str + " movies: "
@@ -130,7 +130,7 @@ def work():
         url = get_movie_url(preurl)
         html = get_html(url)
         movie_list = get_movie_all(html)
-        for movie in movie_list:  # 将每一页中的每个电影信息放入函数中提取
+        for movie in movie_list:  
             result = get_movie_one(movie,name)
             text = str(result[0])  + str(result[1]) + '\n' + '\t'
             save_file(text, '/home/tmp/infocollection/data/IMDB_by_people.txt')
