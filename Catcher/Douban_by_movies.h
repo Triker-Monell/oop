0386c281a60b55c8ccab805cbb21d8747aa33b57
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 22:28:51 +0800

    Update Douban_by_movies.h

--- Catcher/Douban_by_movies.h
+++ Catcher/Douban_by_movies.h
@@ -1,5 +1,8 @@
 #ifndef MICP_DOUBANCATCHER_H
 #define MICP_DOUBANCATCHER_H
+
+//类的具体含义详见Catcher说明文档
+//函数的功能如BaseCatcher中的说明所述
 #include "BaseCatcher.h"
 class Douban_by_movies:public BaseCatcher{
 public:
