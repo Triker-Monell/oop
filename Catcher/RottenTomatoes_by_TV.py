ba56cec272d8e76e675eb0b44a4f6ac37a400937
Monell <37036994+Triker-Monell@users.noreply.github.com>
2018-07-01 23:06:57 +0800

    using

--- Catcher/RottenTomatoes_by_TV.py
+++ Catcher/RottenTomatoes_by_TV.py
@@ -53,7 +53,7 @@ def get_movie_one(movie,name):
         for line in soup_info.stripped_strings:
             result_str=result_str+line+" "
 
-    os.chdir(os.path.join(os.getcwd(), '/home/tmp/infocollection/data/photos'))
+    os.chdir(os.path.join(os.getcwd(), '/tmp/infocollection/data/photos'))
 
     post = soup_all.find('img',class_="posterImage")
     if(post!=None):
@@ -64,7 +64,7 @@ def get_movie_one(movie,name):
 
 
 
-    os.chdir(r'/home/tmp/infocollection/data/')
+    os.chdir(r'/tmp/infocollection/data/')
     result.append(result_str)
 
 
@@ -82,7 +82,7 @@ def read_file(filename):  #读取文件
     return text
 def work():
     try:
-        f = open('/home/tmp/infocollection/data/RottenTomatoes_by_TV.txt', 'r')
+        f = open('/tmp/infocollection/data/RottenTomatoes_by_TV.txt', 'r')
         content=f.read()
         name=content[0:-2]
         name=name.rstrip().replace(' ','_')
@@ -91,7 +91,7 @@ def work():
 
         if f:
             f.close()
-            w = open('/home/tmp/infocollection/data/RottenTomatoes_by_TV.txt', 'w')
+            w = open('/tmp/infocollection/data/RottenTomatoes_by_TV.txt', 'w')
             w.truncate()
             w.close()
         url = 'https://www.rottentomatoes.com/tv/'+name+'/s0'+str(i)
@@ -100,7 +100,7 @@ def work():
         for movie in movie_list:   
             result = get_movie_one(movie,name)
             text = '' + 'TV: ' + str(result[0])  + str(result[1]) + '\n' + '\t'
-            save_file(text, '/home/tmp/infocollection/data/RottenTomatoes_by_TV.txt')
+            save_file(text, '/tmp/infocollection/data/RottenTomatoes_by_TV.txt')
 
 
 if __name__=='__main__':
