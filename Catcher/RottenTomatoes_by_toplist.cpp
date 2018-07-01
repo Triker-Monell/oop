b8115f65d05908e58d9c5c67cd37c9b96f81eb22
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 22:14:05 +0800

    Update RottenTomatoes_by_toplist.cpp

--- Catcher/RottenTomatoes_by_toplist.cpp
+++ Catcher/RottenTomatoes_by_toplist.cpp
@@ -8,6 +8,8 @@ void RottenTomatoes_by_toplist::ExporttoDatabase() {
 
 }
 void RottenTomatoes_by_toplist::MakeCatcher() {
+    //该部分为使用C++中的python指针调用py工作
+    //每个函数只有在PyImport_ImportModule时不同，每个去调用名称对应的xxx.py
     PyObject * pModule = nullptr,*pFunc = nullptr;
     PyObject * pArgs = nullptr,*pValue = nullptr;
     Py_Initialize();
@@ -18,7 +20,7 @@ void RottenTomatoes_by_toplist::MakeCatcher() {
     qDebug() << QString::fromStdString(cmd);
 
     PyRun_SimpleString(cmd.c_str());
-    //????????
+     
 
     pModule = PyImport_ImportModule("RottenTomatoes_by_toplist");
     pFunc = PyObject_GetAttrString(pModule, "work");
