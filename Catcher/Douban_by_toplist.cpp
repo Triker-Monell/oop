7f83cd1acece6d78f26254f72e6e0df2146679aa
Mehooz <37171450+Mehooz@users.noreply.github.com>
2018-07-01 22:10:49 +0800

    Update Douban_by_toplist.cpp

--- Catcher/Douban_by_toplist.cpp
+++ Catcher/Douban_by_toplist.cpp
@@ -10,6 +10,8 @@ void Douban_by_toplist::ExporttoDatabase() {
 
 }
 void Douban_by_toplist::MakeCatcher() {
+    //该部分为使用C++中的python指针调用py工作
+    //每个函数只有在PyImport_ImportModule时不同，每个去调用名称对应的xxx.py
     PyObject * pModule = nullptr,*pFunc = nullptr;
     PyObject * pArgs = nullptr,*pValue = nullptr;
     Py_Initialize();
@@ -20,8 +22,8 @@ void Douban_by_toplist::MakeCatcher() {
     qDebug() << QString::fromStdString(cmd);
 
     PyRun_SimpleString(cmd.c_str());
-    //????????
-
+    
+ 
     pModule = PyImport_ImportModule("Douban_by_toplist");
     pFunc = PyObject_GetAttrString(pModule, "work");
     pValue = PyObject_CallObject(pFunc,pArgs);
